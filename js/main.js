11b1f070a90f392e50b525748f9b426c44e7b7bf
Sajjad <sajjad.saberi1996@gmail.com>
2023-12-14 21:13:57 +0330

    new train of forEach

--- js/main.js
+++ js/main.js
@@ -19,3 +19,15 @@ const arr = [2,4,16,88,10,9,0,64] ;
 const result = arr.reduce((accumulator,currentItem) => accumulator + currentItem,0) ;
 console.log(result);
 
+//
+
+const arry = [1,3,5,7,9,11,13,15,17,19] ;
+
+const container = [] ;
+
+arry.forEach((number, index) => {
+    console.log("number is :" + number);
+    console.log("index is :" + index) ;
+}) ;
+
+console.log(arry) ;
