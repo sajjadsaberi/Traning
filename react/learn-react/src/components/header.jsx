4536fcf2acc25d178a72857e4da2c535e871164d
Sajjad <sajjad.saberi1996@gmail.com>
2023-12-17 20:35:51 +0330

    Adjusent Tag in React JS

--- react/learn-react/src/components/header.jsx
+++ react/learn-react/src/components/header.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 function Headers() {
     return(
+        <>
         <div>
             <p>Hello World!</p>
             <p>{Math.floor(Math.random()* 100)}</p>
         </div>
+        <div>
+            <h3>ID : Sajjad.Front</h3>
+        </div>
+        </>
     );
 };
 
