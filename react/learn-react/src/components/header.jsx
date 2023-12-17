e88ce991ef4e8fe12d7991176ce724e8b5e58d25
Sajjad <sajjad.saberi1996@gmail.com>
2023-12-17 20:23:57 +0330

    The Dynamic content in react js

---
+++ react/learn-react/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Headers() {
+    return(
+        <div>
+            <p>Hello World!</p>
+            <p>{Math.floor(Math.random()* 100)}</p>
+        </div>
+    );
+};
+
+export default Headers ;
